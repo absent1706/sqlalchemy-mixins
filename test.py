8ec963365d5d65493a148a614dae195873be09b6
Alexander Litvinenko <litvinenko1706@gmail.com>
2017-03-13 21:58:19 +0200

    ~~ coverage options

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import unittest
-
-loader = unittest.TestLoader()
-tests = loader.discover(os.path.join(os.path.dirname(__file__),
-                                     'sqlalchemy_mixins'))
-testRunner = unittest.runner.TextTestRunner()
-testRunner.run(tests)
