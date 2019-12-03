3224ef9b6125fe918fac3e2fcd25fb7fec597aba
Farid Rener <proteusvacuum@gmail.com>
2019-12-03 15:03:13 -0500

    Add ability to use date comparison operators

--- sqlalchemy_mixins/smartquery.py
+++ sqlalchemy_mixins/smartquery.py
@@ -159,8 +159,22 @@ class SmartQueryMixin(InspectionMixin, EagerLoadMixin):
         'iendswith': lambda c, v: c.ilike('%' + v),
 
         'year': lambda c, v: extract('year', c) == v,
+        'year_gt': lambda c, v: extract('year', c) > v,
+        'year_ge': lambda c, v: extract('year', c) >= v,
+        'year_lt': lambda c, v: extract('year', c) < v,
+        'year_le': lambda c, v: extract('year', c) <= v,
+
         'month': lambda c, v: extract('month', c) == v,
-        'day': lambda c, v: extract('day', c) == v
+        'month_gt': lambda c, v: extract('month', c) > v,
+        'month_ge': lambda c, v: extract('month', c) >= v,
+        'month_lt': lambda c, v: extract('month', c) < v,
+        'month_le': lambda c, v: extract('month', c) <= v,
+
+        'day': lambda c, v: extract('day', c) == v,
+        'day_gt': lambda c, v: extract('day', c) > v,
+        'day_ge': lambda c, v: extract('day', c) >= v,
+        'day_lt': lambda c, v: extract('day', c) < v,
+        'day_le': lambda c, v: extract('day', c) <= v,
     }
 
     @classproperty
