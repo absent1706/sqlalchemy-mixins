7e27c573171acf3e09958e1686d8d6551bf9a316
Oleksandr Lytvynenko <oleksandr.lytvynenko@codeit.pro>
2020-03-27 12:23:04 +0200

    + added TimestampsMixin to AllFeaturesMixin; ~ updated examples/all_features.py

--- examples/all_features.py
+++ examples/all_features.py
@@ -52,7 +52,7 @@ class Comment(BaseModel):
     user = sa.orm.relationship('User')
 
 
-engine = sa.create_engine('sqlite:///:memory:', echo=True)
+engine = sa.create_engine('sqlite:///:memory:', echo=False)
 session = scoped_session(sessionmaker(bind=engine))
 
 Base.metadata.create_all(engine)
@@ -67,16 +67,29 @@ post2 = Post.create(body='long-long-long-long-long body', rating=2,
 # filter using operators like 'in' and 'contains' and relations like 'user'
 # will output this beauty: <Post #1 body:'Post1' user:'Bill'>
 print(Post.where(rating__in=[2, 3, 4], user___name__like='%Bi%').all())
+
 # joinedload post and user
 print(Comment.with_joined('user', 'post', 'post.comments').first())
+
 # subqueryload posts and their comments
 print(User.with_subquery('posts', 'posts.comments').first())
+
 # sort by rating DESC, user name ASC
 print(Post.sort('-rating', 'user___name').all())
-# {'id': 1, 'body': 'Post 1', 'user_id': 1}
-print(post1.to_dict())
+
+# {'id': 1, 'body': 'Post 1', 'user_id': 1, 'created_at': ..., 'updated_at': ...}
+print('User serialized (no relationships):', post1.to_dict())
+
 # {'id': 1,
 # 'name': 'Bob',
+# 'created_at': datetime.datetime(2020, 3, 27, 10, 19, 33, 655841),
+# 'updated_at': datetime.datetime(2020, 3, 27, 10, 19, 33, 655841)
 # 'posts': [{'body': 'Post 1', 'id': 1, 'user_id': 1},
 #           {'body': 'Post 2', 'id': 2, 'user_id': 1}]}
-print(bob.to_dict(nested=True))
+print('User serialized with relationships:', bob.to_dict(nested=True))
+
+# Created Bob:     2020-03-27 10:19:33.655841
+print("Created Bob:    ", bob.created_at)
+bob.update(name='Bobby')
+# Updated Bob:     2020-03-27 10:19:33.705463
+print("Updated Bob:    ", bob.updated_at)
--- sqlalchemy_mixins/__init__.py
+++ sqlalchemy_mixins/__init__.py
@@ -12,6 +12,6 @@ from .timestamp import TimestampsMixin
 
 
 # all features combined to one mixin
-class AllFeaturesMixin(ActiveRecordMixin, SmartQueryMixin, ReprMixin, SerializeMixin):
+class AllFeaturesMixin(ActiveRecordMixin, SmartQueryMixin, ReprMixin, SerializeMixin, TimestampsMixin):
     __abstract__ = True
     __repr__ = ReprMixin.__repr__
