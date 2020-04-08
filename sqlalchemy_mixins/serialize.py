2c8af217643d12f721ab5e261a0fb44fe99d397c
Michael Bukachi <michaelbukachi@gmail.com>
2020-04-09 00:15:02 +0300

    feat: Add support for hybrid attributes during serialization

--- sqlalchemy_mixins/serialize.py
+++ sqlalchemy_mixins/serialize.py
@@ -8,24 +8,29 @@ class SerializeMixin(InspectionMixin):
 
     __abstract__ = True
 
-    def to_dict(self, nested=False):
+    def to_dict(self, nested=False, include_hybrid=False):
         """Return dict object with model's data.
 
         :param nested: flag to return nested relationships' data if true
         :type: bool
+        :param include_hybrid: flag to include hybrid attributes if true
         :return: dict
         """
         result = dict()
         for key in self.columns:
             result[key] = getattr(self, key)
 
+            if include_hybrid:
+                for key in self.hybrid_properties:
+                    result[key] = getattr(self, key)
+
         if nested:
             for key in self.relations:
                 obj = getattr(self, key)
 
                 if isinstance(obj, SerializeMixin):
-                    result[key] = obj.to_dict()
+                    result[key] = obj.to_dict(include_hybrid=include_hybrid)
                 elif isinstance(obj, Iterable):
-                    result[key] = [o.to_dict() for o in obj]
+                    result[key] = [o.to_dict(include_hybrid=include_hybrid) for o in obj]
 
         return result
