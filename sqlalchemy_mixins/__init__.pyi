285a53037a7b961bbc36304bab6a975961f00a8d
Aurthur Musendame <aurthurmusendame@gmail.com>
2024-01-28 16:42:42 +0200

    upated __init__.pyi for import references to work well

--- sqlalchemy_mixins/__init__.pyi
+++ sqlalchemy_mixins/__init__.pyi
@@ -1,9 +1,31 @@
-from .serialize import SerializeMixin
+from .session import SessionMixin
+from .inspection import InspectionMixin
+from .activerecord import ActiveRecordMixin, ModelNotFoundError
+from .activerecordasync import ActiveRecordMixinAsync
+from .smartquery import SmartQueryMixin, smart_query
+from .eagerload import EagerLoadMixin, JOINED, SUBQUERY
 from .repr import ReprMixin
-from .smartquery import SmartQueryMixin
-from .activerecord import ActiveRecordMixin
+from .serialize import SerializeMixin
+from .timestamp import TimestampsMixin
 
 
 class AllFeaturesMixin(ActiveRecordMixin, SmartQueryMixin, ReprMixin, SerializeMixin):
     __abstract__ = True
-    __repr__ = ReprMixin.__repr__  # type: ignore
+    __repr__ = ReprMixin.__repr__
+
+__all__ = [
+    'SessionMixin',
+    'InspectionMixin',
+    'ActiveRecordMixin',
+    'ModelNotFoundError',
+    'ActiveRecordMixinAsync',
+    'SmartQueryMixin',
+    'smart_query',
+    'EagerLoadMixin',
+    'JOINED',
+    'SUBQUERY',
+    'ReprMixin',
+    'SerializeMixin',
+    'TimestampsMixin',
+    'AllFeaturesMixin',
+]
